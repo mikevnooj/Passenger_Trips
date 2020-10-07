77375957dbd9597c42b8dc246929a08fca208adc
Mike Nugent, Jr <michaelvnugent@gmail.com>
2020-10-06 23:24:20 -0400

    add aaron by day by bus

--- src/Ad_hoc_ridership_requests.R
+++ src/Ad_hoc_ridership_requests.R
@@ -95,6 +95,8 @@
 # 2020/10/01
 # mr. roth wants monthly total wheelchair users, by route, excluding route 90
 
+# 2020/10/06
+# aaron wants to know apc count by day by vehicle for June 2020
 
 ### --- Libraries --- ###
 
@@ -7641,20 +7643,193 @@ FactFare_TC %>%
     gt::gt()
   
 
-FactFare_dates_no_zero_joined[Transit_Day >= "2020-09-01",.N*FareCount,FareCount]
-
-
+# 2020/10/06 Aaron Vogel --------------------------------------------------
+  library(dplyr)
+  library(timeDate)
+  library(data.table)
+  library(lubridate)
+  library(stringr)
+  library(ggplot2)
+  library(leaflet)
+  #first we'll get passenger trips from VMH
+  con <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "REPSQLP01VW", 
+                        Database = "TransitAuthority_IndyGo_Reporting", 
+                        Port = 1433)
   
-   ][
-              #filter for time frame and for service type
-              Transit_Day >= ymd(VMH_StartTime) & 
-                Transit_Day <= ymd(VMH_EndTime) &
-                Service_Type == "Weekday"
-              ][
-                #get avg boardings
-                ,.(
-                  Average_Daily_Wheelchair_Users = round(mean(Daily_Wheelchair_Users))
-                )
-                ,.(StopDesc_trimmed,Service_Type)
-                ][StopDesc_trimmed %ilike% "(Verm|State|9th|IU|18|22|Fall|34|38|Mea|Key).*ion$"]
+  VMH_StartTime <- "20200601"
+  VMH_EndTime <- "20200630"
+  
+  
+  #grab 90 and 901/902 just in case
+  #and also get stops
+  VMH_Raw <- tbl(
+    con,
+    sql(
+      paste0(
+        "select Vehicle_ID
+      ,a.Time
+      ,Route
+      ,Trip
+      ,Route_Name
+      ,Boards
+      ,Alights
+      ,Stop_Id
+      ,Latitude
+      ,Longitude
+      ,GPSStatus
+      from avl.Vehicle_Message_History a (nolock)
+      left join avl.Vehicle_Avl_History b
+      on a.Avl_History_Id = b.Avl_History_Id
+      where a.Route like '90%'
+      and a.Time > '",VMH_StartTime,"'
+      and a.Time < DATEADD(day,1,'",VMH_EndTime,"')
+      and (Boards > 0 OR Alights > 0)
+      and Stop_Id <> 0
+      "
+      )#end paste
+    )#endsql
+  ) %>% #end paste0
+    collect() %>%
+    setDT()
+  
+  #fwrite(VMH_Raw,"data//VMH_Raw.csv")
+  
+  #VMH_Raw <- fread("data//VMH_Raw.csv")
+  
+  #do Transit_Day and Service Type
+  #get holidays
+  #set the holy days
+  holidays_sunday_service <- c("USNewYearsDay", "USMemorialDay",
+                               "USIndependenceDay", "USLaborDay",
+                               "USThanksgivingDay", "USChristmasDay")
+  
+  holidays_saturday_service <- c("USMLKingsBirthday")
+  
+  #set sat sun
+  holidays_sunday <- holiday(2000:2020, holidays_sunday_service)
+  holidays_saturday <- holiday(2000:2020, holidays_saturday_service)
+  #set service type column
+  
+  VMH_Raw[
+    #prepare date and time
+    , c("ClockTime","Date") := list(as.ITime(str_sub(Time, 12, 19)),as.IDate(str_sub(Time, 1, 10)))
+    ][
+      #label prev day or current
+      , DateTest := ifelse(ClockTime<"03:00:00",1,0)
+      ][
+        , Transit_Day := fifelse(
+          DateTest ==1
+          ,as_date(Date)-1
+          ,as_date(Date)
+        )#end fifelse
+        ][
+          ,Transit_Day := as_date("1970-01-01")+days(Transit_Day)
+          ][
+            ,Service_Type := case_when(Transit_Day %in% as_date(holidays_saturday@Data) ~ "Saturday"
+                                       ,Transit_Day %in% as_date(holidays_sunday@Data) ~ "Sunday"
+                                       ,weekdays(Transit_Day) %in% c("Monday","Tuesday","Wednesday","Thursday","Friday")~"Weekday"
+                                       ,TRUE ~ weekdays(Transit_Day)) 
+            ]
+  
+  
+  #clean VMH
+  
+  VMH_joined_daily <- VMH_Raw[
+    ,.(
+      Boardings = sum(Boards)
+      , Alightings = sum(Alights)
+    )
+    ,.(Transit_Day,Vehicle_ID)
+    ]
+  
+  VMH_joined_zero <- VMH_joined_daily[Boardings == 0 | Alightings == 0]
+  
+  #remove zero board or alight veh
+  VMH_no_zero <- VMH_Raw[!VMH_joined_zero,on = c(Transit_Day = "Transit_Day",Vehicle_ID = "Vehicle_ID")]
+  
+  
+  #graph for obvious outliers
+  VMH_joined_daily[!VMH_joined_zero,on = c(Transit_Day = "Transit_Day",Vehicle_ID = "Vehicle_ID")] %>%
+    ggplot(aes(x=Boardings)) +
+    geom_histogram(binwidth = 1) +
+    stat_bin(binwidth = 1, geom = "text", aes(label = ..x..), vjust = -1.5)
+  
+  obvious_outlier <- 260
+  
+  outlier_apc_vehicles <- VMH_joined_daily[Boardings > obvious_outlier]
+  
+  
+  #get vehicles < 3 SD from mean
+  #get 3 sd from mean
+  #remove outliers, then also vehicles that are three deeves AND have Boardings/alightings diff of greater than 5%
+  three_deeves <- VMH_no_zero[!outlier_apc_vehicles,on = c("Transit_Day","Vehicle_ID")
+  ][
+    ,.(
+      sum(Boards)
+      ,sum(Alights)
+      )
+    ,.(Transit_Day,Vehicle_ID)
+    ][
+      ,sd(V1)*3]+
+    VMH_no_zero[!outlier_apc_vehicles,on = c("Transit_Day","Vehicle_ID")
+][,.(
+        sum(Boards)
+        ,sum(Alights)
+        )
+      ,.(Transit_Day,Vehicle_ID)
+      ][,mean(V1)]
+  
+  VMH_joined_daily[
+    ,`:=`(
+      pct_diff = (Boardings - Alightings)/((Boardings + Alightings)/2) 
+    )
+    ]
+  
+  #get deeve and big pct
+  VMH_three_deeves_big_pct <- VMH_joined_daily[(Boardings > three_deeves & pct_diff > 0.1) |
+                                                 (Boardings > three_deeves & pct_diff < -0.1)]
+  
+  
+  
+  VMH_clean <- VMH_no_zero[
+    
+    #remove three deeves
+    !VMH_three_deeves_big_pct, on = c(Transit_Day = "Transit_Day",Vehicle_ID = "Vehicle_ID")
+  ][
+    #remove outliers
+    !outlier_apc_vehicles, on = c("Transit_Day","Vehicle_ID")
+  ][
+    #remove 1899 on 2020-06-30
+    Vehicle_ID != 1899
+  ][
+    #remove this vehicle this day as well
+    !VMH_joined_daily[Vehicle_ID==1992 & Transit_Day == "2020-06-06"]
+    , on = c("Transit_Day","Vehicle_ID")
+  ]
+  
+  
+  
+  VMH_Aaron_Summary <-VMH_clean[
+    ,.(Boardings = sum(Boards),
+       Alightings = sum(Alights))
+    ,.(Transit_Day,Vehicle_ID) 
+  ]
+  
+  fwrite(VMH_Aaron_Summary,"data//VMH_Aaron_Summary.csv")
+  
+  #the following are "bad"
+  # Transit_Day Vehicle_ID Boardings
+  # 1:  2020-06-04       1899       403
+  # 2:  2020-06-26       1981       208
+  # 3:  2020-06-28       1992       266
+  # 4:  2020-06-29       1992       215
+  # 5:  2020-06-29       1978       246
+  # 6:  2020-06-29       1986       315
+  # 7:  2020-06-30       1977       242
+  # 8:  2020-06-30       1899       203
+  
+  
+  
+  
+  
   
