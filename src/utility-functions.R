965da208708bdfd4708cbc9992142e9e244749d5
Mike Nugent, Jr <michaelvnugent@gmail.com>
2021-03-17 16:53:23 -0400

    add utility functions script

---
+++ src/utility-functions.R
@@ -0,0 +1,14 @@
+
+# add_transit_day -------------------------------------------------------------
+
+#' Add a new column with transit_day 
+#'
+#'@param x An object coercable to a data.table
+
+add_transit_day <- function(x,){
+  dt <- as.data.table()
+  
+  
+}
+
+
