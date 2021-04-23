f1d36509bc9aa7484c503c7f2780c1f955d59222
Mike Nugent, Jr <michaelvnugent@gmail.com>
2021-04-23 18:10:23 -0400

    finish unclean 90

--- src/red_line_passenger_trips.R
+++ src/red_line_passenger_trips.R
@@ -19,9 +19,9 @@ con_rep <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "REPSQLP
 
 # Time --------------------------------------------------------------------
 
-this_month_Avail <- lubridate::floor_date(Sys.Date()-365, unit = "month")
+this_month_Avail <- lubridate::floor_date(Sys.Date(), unit = "month")
 
-last_month_Avail  <- lubridate::floor_date(lubridate::floor_date(Sys.Date()-365
+last_month_Avail  <- lubridate::floor_date(lubridate::floor_date(Sys.Date()
                                                                  ,unit = "month"
                                                                  ) - 1 #end floor_date
                                            ,unit = "month"
@@ -30,14 +30,14 @@ last_month_Avail  <- lubridate::floor_date(lubridate::floor_date(Sys.Date()-365
 month <- "2020-03-01"
 
 this_month_Avail <- lubridate::floor_date(x = lubridate::as_date(month)
-                                          , unit = "month"
-                                          )
+                                         , unit = "month"
+                                         )
 
 last_month_Avail <- lubridate::floor_date(lubridate::floor_date(lubridate::as_date(month)
-                                                                , unit = "month"
-                                                                ) - 1 #end lubridate::floor_date
-                                          ,unit = "month"
-                                          )#end lubridate::floor_date
+                                                               , unit = "month"
+                                                               ) - 1 #end lubridate::floor_date
+                                         ,unit = "month"
+                                         )#end lubridate::floor_date
 
 
 
@@ -73,6 +73,7 @@ VMH_Raw <- tbl(
     ,Longitude
     ,GPSStatus
     ,CommStatus
+    ,a.Avl_History_Id
     from avl.Vehicle_Message_History a (nolock)
     left join avl.Vehicle_Avl_History b
     on a.Avl_History_Id = b.Avl_History_Id
@@ -96,13 +97,14 @@ VMH_Raw <- tbl(
     ,Longitude
     ,GPSStatus
     ,CommStatus
+    ,a.Avl_History_Id
     from avl.Vehicle_Message_History a (nolock)
     left join avl.Vehicle_Avl_History b
     on a.Avl_History_Id = b.Avl_History_Id
     where a.Time > '",VMH_StartTime,"'
     and a.Time < DATEADD(day,1,'",VMH_EndTime,"')
-    and Vehicle_ID >= 1970
-    and Vehicle_ID <= 1999
+    and Vehicle_ID > 1950
+    and Vehicle_ID < 2000
     
     UNION
     
@@ -120,6 +122,7 @@ VMH_Raw <- tbl(
       ,Longitude
       ,GPSStatus
       ,CommStatus
+      ,a.Avl_History_Id
       from avl.Vehicle_Message_History a (nolock)
       left join avl.Vehicle_Avl_History b
       on a.Avl_History_Id = b.Avl_History_Id
@@ -143,9 +146,6 @@ VMH_Raw_90 <- VMH_Raw[Latitude > 39.709468 & Latitude < 39.877512
 
 Transit_Day_Cutoff <- as.ITime("03:30:00")
 
-
-
-
 VMH_Raw_90[
   , DateTest := fifelse(data.table::as.ITime(Time) < Transit_Day_Cutoff
                         , 1
@@ -157,6 +157,10 @@ VMH_Raw_90[
                              ,data.table::as.IDate(Time)
                              )
     ]
+
+VMH_Raw_90 <- VMH_Raw_90[Transit_Day >= last_month_Avail & Transit_Day < this_month_Avail]
+
+#VMH_Raw_90[Vehicle_ID ==  1984,.(Boards = sum(Boards)),.(Day = Transit_Day)][order(Day)]
 
 #add seconds since midnight for later
 
@@ -238,7 +242,7 @@ VMH_Raw_90[
 
 VMH_Raw_90[ , uniqueN(AdHocTripNumber)]
 #different than sams but its fine maybe
-
+#this is not fine lol
 
 # end detour --------------------------------------------------------------
 
@@ -256,6 +260,8 @@ zero_b_a_vehicles <- VMH_Raw_90[
 ][
   order(Transit_Day)
 ]
+
+
 
 
 # detour ------------------------------------------------------------------
@@ -295,6 +301,7 @@ VMH_Raw_90_no_zero <- VMH_Raw_90[
 #   ,.(Transit_Day,Vehicle_ID)] %>%
 #   View()
 
+
 #get outliers
 VMH_Raw_90_no_zero[
   , .(Boards = sum(Boards)
@@ -305,7 +312,7 @@ VMH_Raw_90_no_zero[
   geom_histogram(binwidth = 1) +
   stat_bin(binwidth = 1, geom = "text", aes(label = ..x..), vjust = -1.5)
 
-VMH_Raw_90_no_zero[Boards == 255 | Alights == 255]
+
   
 #adjust this!!!!!!!!!!!!
 obvious_outlier <- 500
@@ -360,31 +367,118 @@ three_sd_or_pct_vmh <- VMH_Raw_90_no_zero[
   Boardings > three_deeves & pct_diff < -0.1
 ]
 
+VMH_Raw_90_no_zero[
+  #remove outlier apc vehicles
+  !outlier_apc_vehicles
+  ,on = c("Transit_Day","Vehicle_ID")
+  ][
+    ,.(
+      Boardings = sum(Boards)
+      ,Alightings = sum(Alights)
+    )
+    ,.(Transit_Day,Vehicle_ID)
+    ][
+      ,pct_diff := (Boardings - Alightings)/ ((Boardings + Alightings)/2)
+      ][]
+
+VMH_Raw_90_no_zero[]
+
 #get clean vmh
 VMH_90_clean <- VMH_Raw_90[
   !zero_b_a_vehicles
   ,on = c("Transit_Day","Vehicle_ID")
-][
-  !three_sd_or_pct_vmh
-  ,on = c("Transit_Day","Vehicle_ID")
-]
+  ][
+    !three_sd_or_pct_vmh
+    ,on = c("Transit_Day","Vehicle_ID")
+    ]
 
 #get invalid vmh
 VMH_90_invalid <- fsetdiff(VMH_Raw_90,VMH_90_clean)
 
 
 # expansion method --------------------------------------------------------
-valid_dt <- 1
+valid_dt <- VMH_90_clean[, trip_start_hour := hour(min(Time))
+             , AdHocTripNumber
+             ][
+               , .(VBoard = sum(Boards)
+                   , VTrip = uniqueN(AdHocTripNumber))
+               , .(Inbound_Outbound
+                   , trip_start_hour
+                   , Service_Type
+                   )
+               ]
 
-VMH_90_clean %>%
-  group_by(AdHocTripNumber) %>%
-  arrange(seconds_since_midnight) %>%
-  mutate(trip_start_hour = first(Clock_Hour)) %>%
-  arrange(AdHocTripNumber,seconds_since_midnight) %>%
-  group_by(Inbound_Outbound,trip_start_hour,Service_Type) %>%
-  summarise(VBoard = sum(Boards)
-            ,VTrip = n_distinct(AdHocTripNumber))
+invalid_dt <- VMH_90_invalid[, trip_start_hour := hour(min(Time))
+               , AdHocTripNumber
+               ][
+                 , .(IVBoard = sum(Boards)
+                     , IVTrip = uniqueN(AdHocTripNumber))
+                 , .(Inbound_Outbound
+                     , trip_start_hour
+                     , Service_Type
+                 )
+                 ]
 
+month_results <- merge.data.table(valid_dt
+                                  , invalid_dt
+                                  , by = c("Inbound_Outbound","trip_start_hour","Service_Type")
+                                  , all.x = TRUE
+                                  )
+
+month_results[,Boardings_Per_VTrip := VBoard/VTrip
+              ][, `:=` (IVTrip = fifelse(is.na(IVTrip)
+                                         , 0
+                                         , IVTrip
+                                         )
+                        , IVBoard = fifelse(is.na(IVBoard)
+                                            , 0
+                                            , IVBoard
+                                            )
+                        )
+              ][, Boardings_to_add := IVTrip * Boardings_Per_VTrip
+                ][, `:=` (Expanded_Boardings = Boardings_to_add + VBoard
+                          , Percent_IVTrip = IVTrip / (IVTrip + VTrip)
+                          )
+                  ]
+
+
+
+month_results[,sum(VTrip)/(sum(VTrip)+sum(IVTrip))]
+
+sum(month_results[,Expanded_Boardings])
+
+VMH_90_clean[Route == 90
+             , sum(Boards)
+             ]
+
+VMH_90_clean[Stop_Id == 0
+             ,sum(Boards)
+             ]
+
+Service_Day_Set <- VMH_Raw_90[,.(Service_Days_in_Month = uniqueN(Transit_Day))
+                              ,Service_Type
+                              ]
+
+
+month_service_summary <- Service_Day_Set[
+  month_results[, .(Total_Boardings = sum(Expanded_Boardings))
+                , Service_Type
+                ]
+  ,on = c("Service_Type")
+  ][,`:=`(Total_Boardings = round(Total_Boardings)
+          , Average_Daily_Boardings = round(Total_Boardings / Service_Days_in_Month
+                                            ,0)
+          )
+    ][]
+
+month_service_summary
+
+month_total_boardings <- round(month_results[,sum(Expanded_Boardings)],0)
+
+sample_n(VMH_90_clean,1000) %>%
+  leaflet() %>%
+  addCircles() %>%
+  addTiles()
 
 # find problem trips ------------------------------------------------------
 
@@ -453,6 +547,10 @@ VMH_Raw_test[
     ,Vehicle_ID
   )
 ]
+
+VMH_Raw_test[,sum(Boards),Route]
+
+VMH_Raw_test
 
 # how many trips have very few gps reports
  
@@ -778,6 +876,9 @@ joined <- FactSegmentAdherenceRaw %>%
 joined[VehicleReportLabel == 1993 & TripReportLabel %like% 2348
        ][,.(StopReportLabel,ArriveTimeActual,DepartTimeActual)
          ][order(DepartTimeActual)]
+
+
+
 
 # it is not. this is a problem lol
 
